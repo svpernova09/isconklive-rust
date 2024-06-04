7ab8d8801531c89a6ab54b10be2b458ec58c20ed
Joe Ferguson <joe@joeferguson.me>
2024-06-04 13:02:37 -0500

    Update loging

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             println!("Discord Webhook sent. alert_sent: {}", alert_sent);
         } else if user_count < 50 {
             alert_sent = false;
-            println!("Usercount below 10. user_count: {}, alert_sent: {}", user_count, alert_sent);
+            println!("Usercount below 50. user_count: {}, alert_sent: {}", user_count, alert_sent);
         }
 
         // Sleep for 5 minutes
